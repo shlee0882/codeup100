193663667e4e0703834730719e9760f28d445089
sanghyun <shlee0882@gmail.com>
2020-05-22 15:50:16 +0900

    practice codeup basic

---
+++ python-code/1046.py
@@ -0,0 +1,6 @@
+a,b,c = input().split();
+a = int(a)
+b = int(b)
+c = int(c)
+print(a+b+c)
+print("%0.1f" % float((a+b+c)/3))
---
+++ python-code/1053.py
@@ -0,0 +1,7 @@
+a = input()
+a = int(a)
+
+if a==0:
+    print("1")
+elif a==1:
+    print("0")

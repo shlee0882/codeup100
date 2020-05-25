2f8d17bc970006000bc72b68ddc13af74bb6d09e
sanghyun <shlee0882@gmail.com>
2020-05-25 13:51:29 +0900

    basic codeup

--- python-code/1059.py
+++ python-code/1059.py
@@ -1,18 +1,10 @@
 x = int(input())
-x2 = '00'
-x2 += format(x, 'b')
 
-print(x2)
+a = bin(x)
+b = int(a, 2)
 
-convert = "0b"
+c = bin(~x)
 
-for i in list(x2):
-    if i=='0':
-        convert += '1'
-    elif i=='1':
-        convert += '0'
-
-convert2 = int(convert, 2)
-print(convert2)
-    
+d = int(c, 2)
+print(d)
 
---
+++ python-code/1060.py
@@ -0,0 +1,15 @@
+a,b = input().split()
+a = int(a)
+b = int(b)
+
+a = bin(a)
+a1 = int(a, 2)
+
+b = bin(b)
+b1 = int(b, 2)
+
+c = bin(a1 &b1)
+
+
+d = int(c,2)
+print(d)
